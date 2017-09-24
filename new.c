167e9fcaae9b66f5fa453d7e257d8317ae4741e9
Juary Rocha <juary@localhost.localdomain>
2017-09-24 15:34:52 -0300

    Test de commit

---
+++ new.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main() {
+    
+ int a , b, x;
+  scanf("%d", &a);
+  scanf("%d", &b);
+  x = a+b;
+  printf("X = %d\n", x);
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+ 
+    return 0;
+}
